ce2b098c459f2fdc94e5e224e635cfe8da887304
lutangar <johan@larriereguichet.fr>
2024-10-22 19:08:08 +0200

    feat(recherche): implement ollama notation service

--- src/infra/ollama/config.ts
+++ src/infra/ollama/config.ts
@@ -1,12 +1,20 @@
 import { system } from './prompts/system';
-import type { CreateRequest } from 'ollama/src/interfaces';
+import type { CreateRequest as OllamaCreateRequest } from 'ollama/src/interfaces';
+import { Ollama } from 'ollama';
+
+export type CreateRequest = OllamaCreateRequest & {
+  stream?: false;
+};
 
 const modelfile = `
-FROM llama3.1
+FROM llama3.2:1b
 SYSTEM "${system}"
 `;
 
 export const modelCreationRequest: CreateRequest = {
   model: 'example',
-  modelfile
+  modelfile,
+  stream: false,
 };
+
+export const ollama = new Ollama({ host: process.env.OLLAMA_HOST })
