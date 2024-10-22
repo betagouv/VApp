1c71138ce0f1c98bcfe609cd04175d0da0df45d0
lutangar <johan@larriereguichet.fr>
2024-10-22 12:02:54 +0200

    chore(db): setup database for scalingo
    chore(database): remove force ssl param

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,14 +1,33 @@
 import { Pool } from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
 import type { DB } from './types';
+import * as url from 'node:url';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Missing DATABASE_URL');
+}
+const params = url.parse(process.env.DATABASE_URL);
+
+if (!params.auth) {
+  throw new Error('Missing `user:password` in DATABASE_URL');
+}
+const auth = params.auth.split(':');
+
+if (!params.pathname) {
+  throw new Error('Missing `database` name in DATABASE_URL');
+}
+
+if (!params.hostname) {
+  throw new Error('Missing `host` name in DATABASE_URL');
+}
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    password: process.env.POSTGRES_PASSWORD || 'example',
-    user: process.env.POSTGRES_USER || 'vapp',
-    database: process.env.POSTGRES_DB || 'vapp',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : 5432,
+    user: auth[0],
+    password: auth[1],
+    database: params.pathname.split('/')[1],
+    host: params.hostname,
+    port: params.port ? Number(params.port) : 5432,
     max: 10
   })
 });
