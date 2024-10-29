f1b61d16eb00769a3b9c0c43ea671f95aba9a47e
lutangar <johan@larriereguichet.fr>
2024-10-29 16:42:55 +0100

    chore(sentry): better Sentry integration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,5 +1,6 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
@@ -10,10 +11,10 @@ const SENTRY_ENV = process.env.SENTRY_ENV || process.env.NEXT_PUBLIC_SENTRY_ENV;
 Sentry.init({
   dsn: SENTRY_DSN ?? '',
   environment: SENTRY_ENV ?? 'development',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.1
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
 });
