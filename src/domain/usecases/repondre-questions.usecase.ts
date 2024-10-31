a535eccb70be8a7f04ec969cafd5c4f53135a8a4
lutangar <johan@larriereguichet.fr>
2024-10-31 09:33:44 +0100

    feat(ollama): simplify questions usecase

--- src/domain/usecases/repondre-questions.usecase.ts
+++ src/domain/usecases/repondre-questions.usecase.ts
@@ -1,7 +1,6 @@
 import { UsecaseInterface } from './usecase.interface';
 import { Projet } from '../models/projet';
 import { QuestionReponse } from '@/domain/models/question-reponse';
-import { Aide } from '@/domain/models/aide';
 import { ReformulationServiceInterface } from '@/domain/services/reformulation-service.interface';
 import { ProjetRepository } from '@/infra/repositories/projet.repository';
 
@@ -11,13 +10,9 @@ export class RepondreQuestionsUsecase implements UsecaseInterface {
     public reformulationService: ReformulationServiceInterface
   ) {}
 
-  public async execute(
-    projet: Projet,
-    aide: Aide,
-    questionsReponses: QuestionReponse[]
-  ): Promise<Projet['description']> {
+  public async execute(projet: Projet, questionsReponses: QuestionReponse[]): Promise<Projet['description']> {
     // ⚠ mutation du projet
-    await this.reformulationService.reformuler(projet, aide, questionsReponses);
+    await this.reformulationService.reformuler(projet, questionsReponses);
     await this.projetRepository.update(projet);
 
     return projet.description;
