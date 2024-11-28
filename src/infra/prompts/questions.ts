24bd8f6f93e5be1236a63a1a5f27e2bfbc82b868
lutangar <johan@larriereguichet.fr>
2024-11-28 08:19:16 +0100

    fix(questions): fix number of questions in prompt

--- src/infra/prompts/questions.ts
+++ src/infra/prompts/questions.ts
@@ -1,6 +1,8 @@
 import { Projet } from '@/domain/models/projet';
 import { Aide } from '@/domain/models/aide';
 
+const nbQuestions = 3;
+
 export const system = `Tu es une IA bienveillante qui aide l'utilisateur à préciser les éléments de son projet en lien avec des aides ou des subventions.
 
 Ton rôle est de poser des questions simples et encourageantes, permettant à l'utilisateur d'explorer son projet plus en détail et en lien avec les critères des aides ou subventions.
@@ -9,7 +11,7 @@ Tu répondras exclusivement au format JSON, en respectant strictement le format 
 
 export const user = ({ description }: Projet, aides: Aide[]) => `**Instruction**
 
-Pose exactement {num_question} questions bienveillantes et constructives en lien avec la list de subvention et le projet. Les questions doivent inciter l’utilisateur à clarifier des détails pratiques ou concrets en lien avec le programme, sans se perdre dans des informations superflues.
+Pose exactement ${nbQuestions} questions bienveillantes et constructives en lien avec la list de subvention et le projet. Les questions doivent inciter l’utilisateur à clarifier des détails pratiques ou concrets en lien avec le programme, sans se perdre dans des informations superflues.
 
   Tu répondras au format JSON suivant : {{"Q1": str, "Q2": str, "Q3": str}}
 
