a07207dd1dc29d608d2c92bf89355946ee8adf6d
lutangar <johan@larriereguichet.fr>
2024-11-30 10:08:55 +0100

    fix(aide): don't add recurrence just yet

--- src/infra/database/migrations/1732886716648_show_more_aid_fields.ts
+++ src/infra/database/migrations/1732886716648_show_more_aid_fields.ts
@@ -5,15 +5,9 @@ export async function up(db: Kysely<any>): Promise<void> {
     .alterTable('aide_table')
     .addColumn('programs', sql`text[]`, (col) => col.defaultTo(sql`array[]::text[]`).notNull())
     .addColumn('categories', sql`text[]`, (col) => col.defaultTo(sql`array[]::text[]`).notNull())
-    .addColumn('recurrence', 'varchar(255)', (col) => col.notNull())
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema
-    .alterTable('aide_table')
-    .dropColumn('programs')
-    .dropColumn('categories')
-    .dropColumn('recurrence')
-    .execute();
+  await db.schema.alterTable('aide_table').dropColumn('programs').dropColumn('categories').execute();
 }
