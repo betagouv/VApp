31806608f1330b2c1e3326a3d127dec97a30f2cd
lutangar <johan@larriereguichet.fr>
2025-02-18 12:14:14 +0100

    fix(api): set invalid JSON error code as 400

--- src/presentation/api/error-handler.ts
+++ src/presentation/api/error-handler.ts
@@ -3,14 +3,20 @@ import { TsRestResponse } from '@ts-rest/serverless';
 import { RequestValidationError } from '@ts-rest/serverless/next';
 import { JsonApiErrorResponse } from '@/presentation/api/json-api/error-response';
 
-export function isRequestValidationError(err: unknown): err is RequestValidationError {
+export function isRequestValidationError(e: unknown): e is RequestValidationError {
   return Boolean(
-    err && (err instanceof RequestValidationError || (err as RequestValidationError).name === 'RequestValidationError')
+    e && (e instanceof RequestValidationError || (e as RequestValidationError).name === 'RequestValidationError')
   );
 }
 
+export function isJsonParseError(e: unknown): e is RequestValidationError {
+  return Boolean(e && (e instanceof SyntaxError || (e as SyntaxError).name === 'SyntaxError'));
+}
+
 export const errorHandler = (e: unknown) => {
-  if (isRequestValidationError(e)) {
+  if (isJsonParseError(e)) {
+    return TsRestResponse.fromJson(JsonApiErrorResponse.fromStatus(400, 'Invalid JSON'), { status: 400 });
+  } else if (isRequestValidationError(e)) {
     return TsRestResponse.fromJson(JsonApiErrorResponse.fromRequestValidationError(e), { status: 400 });
   }
 
